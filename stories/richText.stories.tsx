c973e2ae1d1ef23679287ab2a8578932a9445208
Vladimir Kanubrikov <Vladimir.Kanubrikov@dataart.com>
2019-05-31 10:43:41 +0300

    Create Rich Text component Create Unit Test for Rich Text Component

--- stories/richText.stories.tsx
+++ stories/richText.stories.tsx
@@ -2,13 +2,21 @@ import { storiesOf } from '@storybook/react';
 import React from 'react';
 import RichText from '../src/components/lib/RichText/RichText';
 
-storiesOf('RichText', module)
-  .add('with html', () => <RichText html="<h1>test</h1>" />, {
+const contentHtm = `
+<h1>Header 1</h1>
+<h2>Header 2</h2>
+<p>
+Paragraph
+<a href="http://test.html">Test link</a>
+</p>
+`;
+storiesOf('Components/RichText', module)
+  .add('with html', () => <RichText html={contentHtm} />, {
     info: { inline: false },
   })
   .add(
     'with html and class',
-    () => <RichText html="<h1>test</h1>" className="rich-text" />,
+    () => <RichText html={contentHtm} className="rich-text" />,
     {
       info: { inline: false },
     }
