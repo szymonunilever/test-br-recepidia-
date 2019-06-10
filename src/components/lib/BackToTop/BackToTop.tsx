244f06fbb4b4526eedacc1f6015336d44c0d08f8
Vladimir Kanubrikov <Vladimir.Kanubrikov@dataart.com>
2019-06-10 10:48:31 +0300

    changes according code review

--- src/components/lib/BackToTop/BackToTop.tsx
+++ src/components/lib/BackToTop/BackToTop.tsx
@@ -1,5 +1,5 @@
 import cx from 'classnames';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import smoothScroll from 'smoothscroll-polyfill';
 import { Button } from '../common/Button';
 import theme from './BackToTop.module.scss';
@@ -16,13 +16,22 @@ const BackToTop = ({
   const onClick = () => {
     window.scroll({ top: 0, left: 0, behavior: 'smooth' });
   };
-  document.addEventListener('scroll', (e: Event) => {
-    const element = e.target as Partial<CustomEventTarget>;
-    const top =
-      element && element.scrollingElement
-        ? element.scrollingElement.scrollTop
-        : document.documentElement.scrollTop;
-    setIsHidden(top + hideTopPositionPx <= hideTopPositionPx);
+
+  useEffect(() => {
+    const scrollListener = (e: Event) => {
+      const element = e.target as Partial<CustomEventTarget>;
+      const top =
+        element && element.scrollingElement
+          ? element.scrollingElement.scrollTop
+          : document.documentElement.scrollTop;
+      setIsHidden(top + hideTopPositionPx <= hideTopPositionPx);
+    };
+
+    document.addEventListener('scroll', scrollListener);
+
+    return function cleanup() {
+      document.removeEventListener('scroll', scrollListener);
+    };
   });
 
   return (
