8786c5818c6b2ff4d7d05ed83f6fff9cfa4714d3
Vladimir Kanubrikov <Vladimir.Kanubrikov@dataart.com>
2019-06-18 17:18:59 +0300

    add Recipe Cooking Method Component

---
+++ src/components/lib/RecipeCookingMethod/RecipeCookingMethod.tsx
@@ -0,0 +1,48 @@
+import cx from 'classnames';
+import { sortBy } from 'lodash';
+import React from 'react';
+import { TagName, Text } from '../Text';
+import { RecipeCookingMethodProps } from './models';
+import theme from './RecipeCookingMethod.module.scss';
+
+const RecipeCookingMethod = ({
+  className,
+  content: { title = undefined, subtitle = undefined },
+  methodList = [],
+  titleLevel = 1,
+}: RecipeCookingMethodProps) => {
+  const classWrapper = cx(theme.recipeCookingMethod, className);
+  const list = sortBy(methodList, ['position']);
+
+  const theTitle = title ? (
+    <Text
+      className="recipe-cooking-method__title"
+      // @ts-ignore
+      tag={TagName[`h${titleLevel}`]}
+      text={title}
+    />
+  ) : null;
+  const theSubTitle = subtitle ? (
+    <Text
+      className="recipe-cooking-method__subtitle"
+      // @ts-ignore
+      tag={TagName[`h${titleLevel + 1}`]}
+      text={subtitle}
+    />
+  ) : null;
+  return (
+    <div className={classWrapper}>
+      {theTitle}
+      {theSubTitle}
+      <ol className="recipe-cooking-method__list">
+        {list.map((item: RMSData.CookingMethod) => (
+          <li key={item.position} className="recipe-cooking-method__item">
+            {item.description}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default RecipeCookingMethod;
