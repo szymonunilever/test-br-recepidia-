84e60701b10a284bb8aa345ebdfd3bfb36d6e79c
Alexander Vishnivetskiy <alexander.vishnivetskiy@dataart.com>
2019-06-19 16:51:32 +0300

    Revert "modal component"
    This reverts commit 10f8b94f3113998556e036670b32d8669ad5ace7.

--- stories/checkelem.stories.tsx
+++ stories/checkelem.stories.tsx
@@ -36,6 +36,7 @@ storiesOf(
           delectus."
         value="test2"
         isChecked={true}
+        value="test"
       />
     ),
     {
@@ -49,7 +50,11 @@ storiesOf(
         type={checkElemTypes.radio}
         name="radiotest"
         label="this is label"
+<<<<<<< HEAD
+        value="test"
+=======
         value="test3"
+>>>>>>> develop
       />
     ),
     {
@@ -69,6 +74,7 @@ storiesOf(
           delectus."
         value="test4"
         isChecked={true}
+        value="test"
       />
     ),
     {
@@ -83,19 +89,31 @@ storiesOf(
           type={checkElemTypes.radio}
           name="radiotest"
           label="Option 1"
+<<<<<<< HEAD
+          value="test"
+=======
           value="option1"
+>>>>>>> develop
         />
         <CheckElem
           type={checkElemTypes.radio}
           name="radiotest"
           label="Option 2"
+<<<<<<< HEAD
+          value="test"
+=======
           value="option2"
+>>>>>>> develop
         />
         <CheckElem
           type={checkElemTypes.radio}
           name="radiotest"
           label="Option 3"
+<<<<<<< HEAD
+          value="test"
+=======
           value="option3"
+>>>>>>> develop
         />
       </div>
     ),
