c691ad688c38e9698a83940809b0140fc87e77cb
Daniil Sharko <daniil.sharko@dataart.com>
2019-06-19 17:58:16 +0300

    Pages and Recipes carousel small fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,27 +4,10 @@ import Layout from 'src/components/Layout/Layout';
 import SEO from 'src/components/Seo/Seo';
 import '../scss/main.scss';
 
-import recipes from 'src/components/data/recipes.json';
-import pages from 'src/components/data/pageListing.json';
-import ResipeListing from 'src/components/lib/RecipeListing/RecipeListing';
-import PageListing from 'src/components/lib/PageListing/PageListing';
-import RecipeListing from 'src/components/lib/RecipeListing/RecipeListing';
-import { RecipeItem } from 'src/components/lib/RecipeListing/partials/models';
-import { RecipeListViewType } from 'src/components/lib/RecipeListing';
-
-const recipeListing: RecipeItem[] = recipes.data.allRecipe.edges.map(
-  (item: { node: RecipeItem | any }) => item.node
-);
 const IndexPage = ({ data, location }: IndexPageProps) => {
   return (
     <Layout location={location} title={data.site.siteMetadata.title}>
       <SEO title="Recepedia Home" />
-      <RecipeListing
-        viewType={RecipeListViewType.Carousel}
-        list={recipeListing}
-        content={{ nullResult: { textList: [] } }}
-      />
-      {/* <PageListing list={pages} viewType={'carousel'} initialCount={4} content={{}}/> */}
       Hello Recepedia
     </Layout>
   );
