a1123cfa0af4901ee1ec88f3c2719d0323563723
almarchenko <alexey.marchenko@dataart.com>
2019-06-19 14:53:15 +0300

    modal component

---
+++ src/components/lib/common/Modal/Modal.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { ModalProps } from './models';
+import cx from 'classnames';
+import ReactModal from 'react-modal';
+
+const Modal = ({
+  isOpen,
+  className,
+  close,
+  closeBtn,
+  ...props
+}: ModalProps) => {
+  const wrapClasses = cx(className, 'modal');
+  return (
+    <ReactModal
+      {...props}
+      isOpen={isOpen}
+      portalClassName={wrapClasses}
+      overlayClassName="modal__overlay"
+      className="modal__content"
+      bodyOpenClassName="modal-is-open"
+    >
+      {props.children}
+      <button onClick={close} className="modal__close-btn">
+        {closeBtn || 'Close'}
+      </button>
+    </ReactModal>
+  );
+};
+
+export default Modal;
