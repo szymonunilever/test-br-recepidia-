03b4ee005eff57a7c81a135503e75b154e3fd5cb
Artem Linenko <artem.linenko@dataart.com>
2019-06-24 18:47:20 +0300

    Moved Navigation and Footer to Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,38 @@
-import { Link } from 'gatsby';
 import React from 'react';
+import Search from 'src/components/Search/Search';
+import GlobalFooter from 'src/components/lib/components/GlobalFooter';
+import footerItemsList from 'src/components/data/globalFooterMenu.json';
+import list from 'src/components/data/globalNavigationMenu.json';
+import LogoIcon from 'src/svgs/inline/logo.svg';
+import UnileverLogoIcon from 'src/svgs/inline/unilever-logo.svg';
+import ArrowDownIcon from 'src/svgs/inline/arrow-down.svg';
+import ButtonCloseIcon from 'src/svgs/inline/x-mark.svg';
+import GlobalNavigation from 'src/components/lib/components/GlobalNavigation';
 
 const Layout = ({ title, children }: LayoutProps) => {
   return (
-    <div>
-      <header>
-        <h1>
-          <Link to={'/'}>{title}</Link>
-        </h1>
-      </header>
+    <div className="global-container">
+      <GlobalNavigation
+        logo={{
+          icon: (
+            <LogoIcon style={{ height: '40px' }} className="main-logo__icon" />
+          ),
+          path: '/',
+        }}
+        dropDownIcon={<ArrowDownIcon className="dropdown-icon" />}
+        buttonCloseIcon={ButtonCloseIcon}
+        content={{ list }}
+      >
+        <Search />
+      </GlobalNavigation>
       <main>{children}</main>
+      <GlobalFooter
+        logoIcon={<UnileverLogoIcon text="Unilever Logo" />}
+        content={{
+          list: footerItemsList,
+          copyrightText: 'Unilever 2019',
+        }}
+      />
     </div>
   );
 };
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,48 +2,23 @@ import { graphql } from 'gatsby';
 import React from 'react';
 import Layout from 'src/components/Layout/Layout';
 import SEO from 'src/components/Seo/Seo';
-import GlobalNavigation from 'src/components/lib/components/GlobalNavigation';
 import { Text, TagName } from 'src/components/lib/components/Text';
-import list from 'src/components/data/globalNavigationMenu.json';
-import LogoIcon from 'src/svgs/inline/logo.svg';
-import UnileverLogoIcon from 'src/svgs/inline/unilever-logo.svg';
-import ArrowDownIcon from 'src/svgs/inline/arrow-down.svg';
-import ButtonCloseIcon from 'src/svgs/inline/x-mark.svg';
+
 import { RecipeListing } from 'src/components/lib/components/RecipeListing';
 import Hero from 'src/components/lib/components/Hero';
 import { findPageComponentContent } from 'src/utils';
 import { RecipeItem } from 'src/components/lib/components/RecipeListing/partials';
-import Search from 'src/components/Search/Search';
-import GlobalFooter from 'src/components/lib/components/GlobalFooter';
-import footerItemsList from 'src/components/data/globalFooterMenu.json';
 
 const HomePage = ({ data }: HomePageProps) => {
   const page = data.allPage.edges[0].node;
   const components = page.components.items;
-  // page.components.items = page.components.items.map(item => ({
-  //   ...item,
-  //   content: JSON.parse(item.content),
-  // }));
   const recipes = data.allRecipe.nodes;
 
   return (
     <Layout>
       <SEO title="Recepedia Home" />
-      <GlobalNavigation
-        logo={{
-          icon: (
-            <LogoIcon style={{ height: '40px' }} className="main-logo__icon" />
-          ),
-          path: '/',
-        }}
-        dropDownIcon={<ArrowDownIcon className="dropdown-icon" />}
-        buttonCloseIcon={ButtonCloseIcon}
-        content={{ list }}
-      >
-        <Search />
-      </GlobalNavigation>
-      <Text tag={TagName['h1']} text={data.allPage.edges[0].node.title} />
 
+      <Text tag={TagName['h1']} text={data.allPage.edges[0].node.title} />
       <section>
         <RecipeListing
           content={findPageComponentContent(
@@ -72,14 +47,6 @@ const HomePage = ({ data }: HomePageProps) => {
           viewType="Image"
         />
       </section>
-
-      <GlobalFooter
-        logoIcon={<UnileverLogoIcon text="Unilever Logo" />}
-        content={{
-          list: footerItemsList,
-          copyrightText: 'Unilever 2019',
-        }}
-      />
     </Layout>
   );
 };
