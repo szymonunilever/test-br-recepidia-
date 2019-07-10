5c8595d831850f7257be9e5fae5f31a23537fc56
Artem Linenko <artem.linenko@dataart.com>
2019-07-10 10:40:11 +0300

    Added CounterySelector and BrandSocialChannels to Layout

--- stories/countrySelector.stories.tsx
+++ stories/countrySelector.stories.tsx
@@ -1,77 +1,19 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 import { CountrySelector } from '../src/components/lib/components/CountrySelector';
-import { LanguageEntry } from 'src/components/lib/components/CountrySelector/models';
+import countriesList from 'src/components/data/countrySelector.json';
 
-const list: LanguageEntry[] = [
-  {
-    value: 'Argentina',
-    label: 'AR',
-    path: 'path/to/argentina/page',
-    icon: '&#x1f30e;',
-  },
-  {
-    value: 'Bolivia',
-    label: 'BO',
-    path: 'path/to/bolivia/page',
-    icon: '&#x1f30e;',
-  },
-  {
-    value: 'Brazil',
-    label: 'BR',
-    path: 'path/to/brazil/page',
-    icon: '&#x1f30e;',
-  },
-  {
-    value: 'Colombia',
-    label: 'CO',
-    path: 'path/to/colombia/page',
-    icon: '&#x1f30e;',
-  },
-  {
-    value: 'Chile',
-    label: 'CL',
-    path: 'path/to/chile/page',
-    icon: '&#x1f30e;',
-  },
-  {
-    value: 'Mexico',
-    label: 'MX',
-    path: 'path/to/mexico/page',
-    icon: '&#x1f30e;',
-  },
-  {
-    value: 'Paraguay',
-    label: 'PY',
-    path: 'path/to/paraguay/page',
-    icon: '&#x1f30e;',
-  },
-  {
-    value: 'Puerto rico',
-    label: 'PR',
-    path: 'path/to/puerto-rico/page',
-    icon: '&#x1f30e;',
-  },
-  {
-    value: 'Rep. Dominicana',
-    label: 'DO',
-    path: 'path/to/domimicana/page',
-    icon: '&#x1f30e;',
-  },
-  {
-    value: 'URUGUAY',
-    label: 'UY',
-    path: 'path/to/uruguay/page',
-    icon: '&#x1f30e;',
-  },
-];
 storiesOf('Components/CountrySelector', module)
-  .add('Country selector default', () => <CountrySelector list={list} />, {
-    info: { inline: false },
-  })
+  .add(
+    'Country selector default',
+    () => <CountrySelector list={countriesList} />,
+    {
+      info: { inline: false },
+    }
+  )
   .add(
     'Country selector with selected',
-    () => <CountrySelector selected={list[0]} list={list} />,
+    () => <CountrySelector selected={countriesList[0]} list={countriesList} />,
     {
       info: { inline: false },
     }
