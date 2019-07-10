93130e9034180ed58cba74e028b92384541fe806
Alexander Vishnivetskiy <alexander.vishnivetskiy@dataart.com>
2019-07-10 14:55:08 +0300

    Kritique set location origin added

--- integrations/Kritique/Kritique.tsx
+++ integrations/Kritique/Kritique.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import keys from '../keys.json';
 import Helmet from 'react-helmet';
 
@@ -9,18 +9,28 @@ const Kritique = () => {
     keys.kritique.apiKey
   }&sitesource=${keys.kritique.siteSource}`;
 
+  const [locationOrigin, setLocationOrigin] = useState('');
+
+  useEffect(() => {
+    setLocationOrigin(window.location.origin);
+  }, []);
+
   return (
-    <Helmet
-      script={[
-        {
-          src: 'https://code.jquery.com/jquery-3.4.1.min.js',
-          integrity: 'sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=',
-          crossOrigin: 'anonymous',
-          defer: true,
-        },
-        { id: 'rr-widget', src: kritiqueWidgetSrc, defer: true },
-      ]}
-    />
+    <>
+      {locationOrigin ? (
+        <Helmet
+          script={[
+            {
+              src: 'https://code.jquery.com/jquery-3.4.1.min.js',
+              integrity: 'sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=',
+              crossOrigin: 'anonymous',
+              defer: true,
+            },
+            { id: 'rr-widget', src: kritiqueWidgetSrc, defer: true },
+          ]}
+        />
+      ) : null}
+    </>
   );
 };
 
