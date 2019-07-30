670b0bb9bfff8316d7229ab262d1d4a335e9828d
emartynenko <ievgemiimart@gmail.com>
2019-07-30 15:22:34 +0300

    EAT-509 updated progress bar accessibility

--- src/components/lib/components/Carousel/partials/ProgressBar.tsx
+++ src/components/lib/components/Carousel/partials/ProgressBar.tsx
@@ -6,10 +6,16 @@ const ProgressBar = ({ percentage = 0 }: ProgressBarProps) => {
   return (
     // <div className={styles['progress-bar'] + ' progress-bar'}>
     <div className={styles.progressBar + ' progress-bar'}>
-      <div
+      <progress
+        aria-label="progress"
+        role="progressbar"
         className={styles.progressBar__content + ' progress-bar__content'}
         style={{ width: `${percentage}%` }}
-      />
+        value={`${percentage}`}
+        max="100"
+      >
+        {percentage}% complete
+      </progress>
     </div>
   );
 };
