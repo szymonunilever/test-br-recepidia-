f2f5d1ea3f21bf593d6e8c3cea8f7a77b902b031
emartynenko <ievgemiimart@gmail.com>
2019-08-02 15:44:08 +0300

    Updated Recipe Page carousel items

--- src/templates/RecipePage/RecipePage.tsx
+++ src/templates/RecipePage/RecipePage.tsx
@@ -70,7 +70,7 @@ const RecipePage = ({ pageContext }: RecipePageProps) => {
         }
       }
 
-      allRecipe(limit: 10) {
+      allRecipe(limit: 6) {
         nodes {
           ...RecipeFields
         }
