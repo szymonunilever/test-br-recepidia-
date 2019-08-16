57665f6143ddd47414a951022587171287af2fa8
Artem Linenko <artem.linenko@dataart.com>
2019-08-16 12:30:17 +0300

    taken image from cloudfront

--- scripts/build/createRemoteImageNode.js
+++ scripts/build/createRemoteImageNode.js
@@ -3,19 +3,18 @@ const keys = require('../../integrations/keys.json');
 
 module.exports = async (url, id, staticParams) => {
   const imgNode = await createRemoteFileNode({
-    url: 'https://develop--br-recepedia-com.netlify.com/Recipe-image.jpg',
+    url:
+      'https://d37k6lxrz24y4c.cloudfront.net/v2/e9dc924f238fa6cc29465942875fe8f0/00fd0884-670e-4df4-adbb-ec517ee965c0-600.jpg',
     // 'https://author-dev-headless.unileversolutions.com/api/assets/aem-headless-cms/headless-demo/br/pt/homepage-content/teaser-content/images/teaser-image.png',
     parentNodeId: id,
     ext: '.jpg',
     name: 'image',
-    auth: {
-      // eslint-disable-next-line @typescript-eslint/camelcase
-      htaccess_user: 'unilever',
-      // htaccess_user: keys.AemAssetsCredentials.user,
-      // eslint-disable-next-line @typescript-eslint/camelcase
-      htaccess_pass: '@@Brazil!@@',
-      // htaccess_pass: keys.AemAssetsCredentials.password,
-    },
+    // auth: {
+    //   // eslint-disable-next-line @typescript-eslint/camelcase
+    //   htaccess_user: keys.AemAssetsCredentials.user,
+    //   // eslint-disable-next-line @typescript-eslint/camelcase
+    //   htaccess_pass: keys.AemAssetsCredentials.password,
+    // },
     ...staticParams,
   });
   return imgNode;
