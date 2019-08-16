e51e122e2a6d90a88881be01378c372842938194
Artem Linenko <artem.linenko@dataart.com>
2019-08-16 12:11:21 +0300

    Changed auth way

--- scripts/build/createRemoteImageNode.js
+++ scripts/build/createRemoteImageNode.js
@@ -3,18 +3,19 @@ const keys = require('../../integrations/keys.json');
 
 module.exports = async (url, id, staticParams) => {
   const imgNode = await createRemoteFileNode({
-    url:
-      'https://unilever:@@Brazil!@@@develop--br-recepedia-com.netlify.com/Recipe-image.jpg',
+    url: 'https://develop--br-recepedia-com.netlify.com/Recipe-image.jpg',
     // 'https://author-dev-headless.unileversolutions.com/api/assets/aem-headless-cms/headless-demo/br/pt/homepage-content/teaser-content/images/teaser-image.png',
     parentNodeId: id,
     ext: '.jpg',
     name: 'image',
-    // auth: {
-    //   // eslint-disable-next-line @typescript-eslint/camelcase
-    //   htaccess_pass: keys.AemAssetsCredentials.password,
-    //   // eslint-disable-next-line @typescript-eslint/camelcase
-    //   htaccess_user: keys.AemAssetsCredentials.user,
-    // },
+    auth: {
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      htaccess_pass: 'unilever',
+      // htaccess_pass: keys.AemAssetsCredentials.password,
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      htaccess_user: '@@Brazil!@@',
+      // htaccess_user: keys.AemAssetsCredentials.user,
+    },
     ...staticParams,
   });
   return imgNode;
