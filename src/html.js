f4f43145cc27aa0962dd20f67161a7e5d706bceb
afenton90 <afenton90@gmail.com>
2019-08-22 13:02:04 +0100

    perf(head): removes analytics D2 script
    a script had been added on the advice of some Digital 2 documenation. However said script is
    extremely descructive to perceived performance

--- src/html.js
+++ src/html.js
@@ -64,29 +64,6 @@ export default function HTML(props) {
                 __html: `NREUM.info={beacon:"bam.nr-data.net",errorBeacon:"bam.nr-data.net",licenseKey:"${licenseKey}",applicationID:"${applicationID}",sa:1}`,
               }}
             />
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-                  (function(g, b, d, f) {
-                    (function(a, c, d) {
-                      if (a) {
-                        var e = b.createElement("style");
-                        e.id = c;
-                        e.innerHTML = d;
-                        a.appendChild(e);
-                      }
-                    })(b.getElementsByTagName("head")[0], "at-body-style", d);
-                    setTimeout(function() {
-                      var a = b.getElementsByTagName("head")[0];
-                      if (a) {
-                        var c = b.getElementById("at-body-style");
-                        c && a.removeChild(c);
-                      }
-                    }, f);
-                  })(window, document, "body {opacity: 0 !important}", 3e3);
-                `,
-              }}
-            />
           </>
         )}
         <script
