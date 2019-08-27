c8d293f85f7bb8f9dad9ddbe48bc8db6fbbfe8b4
Ilya Tikhov <itikhov@dataart.com>
2019-08-27 19:00:16 +0200

    Load kritique widget with 10 sec delay

--- integrations/Kritique/Kritique.tsx
+++ integrations/Kritique/Kritique.tsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import keys from '../keys.json';
 import Helmet from 'react-helmet';
 
+// TODO: Integrations should be moved into lib folder
+import { reloadKritiqueWidget } from '../../src/components/lib/utils/useKritiqueReload';
+
 const Kritique = () => {
   const kritiqueWidgetSrc = `${keys.kritique.url}?brandid=${
     keys.kritique.brandId
@@ -10,15 +13,37 @@ const Kritique = () => {
   }&sitesource=${keys.kritique.siteSource}`;
 
   const [locationOrigin, setLocationOrigin] = useState('');
+  const [injectScript, setinjectScript] = useState(false);
 
   useEffect(() => {
     setLocationOrigin(window.location.origin);
+
+    setTimeout(() => setinjectScript(true), 10000);
   }, []);
+
+  // @ts-ignore
+  const handleScriptInject = ({ scriptTags }) => {
+    if (scriptTags) {
+      const scriptTag = scriptTags.find(
+        (el: HTMLElement) => el.id === 'rr-widget'
+      );
+
+      if (scriptTag) {
+        scriptTag.onload = () => setTimeout(reloadKritiqueWidget, 2000);
+      }
+    }
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleChangeClientState = (newState: any, addedTags: any) =>
+    handleScriptInject(addedTags);
 
   return (
     <>
-      {locationOrigin ? (
+      {locationOrigin && injectScript ? (
         <Helmet
+          // @ts-ignore
+          onChangeClientState={handleChangeClientState}
           script={[
             {
               src: '/libs/jquery.min.js',
