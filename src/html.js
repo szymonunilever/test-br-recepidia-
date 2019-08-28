dbbc75a66008abef9735e65c3ab0e12182a1f833
Artem Linenko <artem.linenko@dataart.com>
2019-08-28 14:34:43 +0300

    Remove styles preload, remove home page content

--- src/html.js
+++ src/html.js
@@ -28,7 +28,7 @@ export default function HTML(props) {
         {process.env.NODE_ENV !== 'development' && (
           <>
             <link rel="preload" href="/config/newRelic.js" as="script" />
-            <link rel="preload" href="/styles.css" as="style" />
+            {/* <link rel="preload" href="/styles.css" as="style" /> */}
             <link rel="preload" href="/libs/jquery.min.js" as="script" />
             <link
               rel="preload"
