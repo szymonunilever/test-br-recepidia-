7210b991990e9247f7fd4cb8347489209de78357
Artem Linenko <artem.linenko@dataart.com>
2019-09-04 14:42:07 +0300

    Removed hardcoded placeholder

--- src/components/lib/components/RecipeHero/models.ts
+++ src/components/lib/components/RecipeHero/models.ts
@@ -3,6 +3,5 @@ import { UnileverLibraryComponent } from '../globalModels';
 export interface RecipeHeroProps
   extends UnileverLibraryComponent<Internal.Recipe> {
   className?: string;
-  imagePlaceholder: Internal.LocalImage;
   imageSizes: string;
 }
