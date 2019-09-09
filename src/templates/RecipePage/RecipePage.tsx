b7f1b27b0a1c6ace759a50a9ff4f4913b3c52c3b
vovank86 <vovank86@gmail.com>
2019-09-09 14:04:28 +0300

    EAT-948 "Share this recipe" is not translated

--- src/templates/RecipePage/RecipePage.tsx
+++ src/templates/RecipePage/RecipePage.tsx
@@ -41,7 +41,7 @@ import SocialSharing, {
   SocialIcons,
 } from 'src/components/lib/components/SocialSharing';
 import AddThis from '../../../integrations/AddThis';
-import socialSharingContent from 'src/components/data/socialSharingContent.json';
+// import socialSharingContent from 'src/components/data/socialSharingContent.json';
 import { ReactComponent as FacebookIcon } from 'src/svgs/inline/facebook.svg';
 import { ReactComponent as TwitterIcon } from 'src/svgs/inline/twitter.svg';
 import Hero from 'src/components/lib/components/Hero';
@@ -232,7 +232,7 @@ const RecipePage = ({ pageContext, location }: RecipePageProps) => {
         </div>
         <>
           <SocialSharing
-            content={socialSharingContent}
+            content={findPageComponentContent(components, 'SocialSharing')}
             viewType={SocialSharingViewType.Modal}
             CloseButtonIcon={CloseButton}
             icons={socialIcons}
