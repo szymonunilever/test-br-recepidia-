7dbdb73061d322adf786a4d84b6847bb8efdbe31
Artem Linenko <artem.linenko@dataart.com>
2019-09-09 13:11:12 +0300

    Remove preloads

--- src/html.js
+++ src/html.js
@@ -25,7 +25,7 @@ export default function HTML(props) {
             <link rel="preconnect" href="https://js-agent.newrelic.com" />
 
             {/* START kritique preloads */}
-            <link rel="preload" href="/libs/jquery.min.js" as="script" />
+            {/* <link rel="preload" href="/libs/jquery.min.js" as="script" />
             <link rel="preload" href={kritiqueWidgetSrc} as="script" />
             <link
               rel="preload"
@@ -33,7 +33,7 @@ export default function HTML(props) {
                 keys.kritique.baseUrl
               }/widget/resources/css/RR_widget.css`}
               as="style"
-            />
+            /> */}
             {/* END kritique preloads */}
 
             <script type="text/javascript" dangerouslySetInnerHTML={newRelic} />
