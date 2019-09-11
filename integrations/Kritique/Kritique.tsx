804de2413bf9dfcb11c2d038d6da953984454ac1
Artem Linenko <artem.linenko@dataart.com>
2019-09-11 16:37:18 +0300

    Load Kritique with 100 ms timeout

--- integrations/Kritique/Kritique.tsx
+++ integrations/Kritique/Kritique.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import keys from '../keys.json';
 import Helmet from 'react-helmet';
 
+// TODO: Integrations should be moved into lib folder
+import { reloadKritiqueWidget } from '../../src/components/lib/utils/useKritiqueReload';
 import { isBrowser } from 'src/utils';
+
+export enum ScriptType {
+  jQuery = 'jQuery',
+  Kritique = 'ratingReview',
+}
 
 const Kritique = () => {
   const kritiqueWidgetSrc = `${keys.kritique.url}?brandid=${
@@ -11,12 +18,33 @@ const Kritique = () => {
     keys.kritique.apiKey
   }&sitesource=${keys.kritique.siteSource}`;
 
+  const [injectScript, setInjectScript] = useState(false);
+
+  useEffect(() => {
+    const isKritiqueLoaded = !!sessionStorage.getItem('isKritiqueLoaded');
+
+    if (isKritiqueLoaded) {
+      setInjectScript(isKritiqueLoaded);
+      setTimeout(reloadKritiqueWidget, 2000);
+    } else {
+      setTimeout(() => {
+        sessionStorage.setItem('isKritiqueLoaded', 'true');
+        setInjectScript(true);
+      }, 100);
+    }
+  }, []);
+
   return (
     <>
-      {isBrowser() ? (
+      {isBrowser() &&
+      (!!sessionStorage.getItem('isKritiqueLoaded') || injectScript) ? (
         <Helmet
           // @ts-ignore
           script={[
+            {
+              src: '/libs/jquery.min.js',
+              defer: true,
+            },
             {
               id: 'rr-widget',
               src: kritiqueWidgetSrc,
