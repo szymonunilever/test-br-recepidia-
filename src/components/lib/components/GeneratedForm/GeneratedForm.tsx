c2129f3f024a85a2b55d19c9b861fab9c05f135c
Vladimir Kanubrikov <Vladimir.Kanubrikov@dataart.com>
2019-11-04 19:47:13 +0200

    EAT-1249 [Quiz] Form is not sent after re-pass without editing fields

--- src/components/lib/components/GeneratedForm/GeneratedForm.tsx
+++ src/components/lib/components/GeneratedForm/GeneratedForm.tsx
@@ -1,29 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { GeneratedFormProps } from './models';
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 import FormInstance from './partials/FormInstance';
 
 const GeneratedForm = (props: GeneratedFormProps) => {
-  const [hasCaptcha, setHasCaptcha] = useState(false);
-  useEffect(() => {
-    //@ts-ignore
-    if (window.grecaptcha) {
-      const reCaptcha = document.querySelector('.grecaptcha-badge');
-      if (reCaptcha && reCaptcha.parentNode) {
-        reCaptcha.parentNode.removeChild(reCaptcha);
-      }
-      //@ts-ignore
-      delete window.grecaptcha;
-    }
-    setHasCaptcha(
-      !!props.content.fields.find(
-        (field: AppContent.GeneratedForm.Field) => field.type === 'captcha'
-      )
-    );
-    return function cleanup() {
-      setHasCaptcha(false);
-    };
-  });
+  const [hasCaptcha] = useState(
+    !!props.content.fields.find(
+      (field: AppContent.GeneratedForm.Field) => field.type === 'captcha'
+    )
+  );
   const view = hasCaptcha ? (
     <GoogleReCaptchaProvider reCaptchaKey={process.env['ReCaptcha_clientKey']}>
       <FormInstance {...props} hasCaptcha={hasCaptcha} />
