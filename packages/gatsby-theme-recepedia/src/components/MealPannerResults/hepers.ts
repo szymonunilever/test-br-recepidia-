2c745c3e3f85ca74558f47581696075e51fb5fbd
Vladimir Kanubrikov <Vladimir.Kanubrikov@dataart.com>
2019-12-04 16:36:13 +0200

    EAT-1385 [Meal Planner] First search query is displayed during the second search in heading

---
+++ packages/gatsby-theme-recepedia/src/components/MealPannerResults/hepers.ts
@@ -0,0 +1,34 @@
+import { useElasticSearch } from '../../utils';
+
+export function getCustomMPSearch(
+  searchQuery: string,
+  {
+    from = 0,
+    size = 3000,
+    sort = [{ averageRating: { order: 'desc' } }],
+    fields = ['title'],
+  }
+) {
+  const searchParams = {
+    index: process.env['elasticSearch_recipeIndex'] as string,
+    body: {
+      from,
+      size,
+      sort,
+      query: {
+        bool: {
+          must: {
+            // eslint-disable-next-line @typescript-eslint/camelcase
+            query_string: {
+              // eslint-disable-next-line @typescript-eslint/camelcase
+              analyze_wildcard: true,
+              query: `${searchQuery}*`,
+              fields,
+            },
+          },
+        },
+      },
+    },
+  };
+  return useElasticSearch<Internal.Recipe>(searchParams);
+}
